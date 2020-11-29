0c0ad04fa4914c99c8bfde1c8ea7aded3ccbd849
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2020-11-29 04:44:18 -0300

    Create ke.bat
    File which redirects to wadb commands, wsldl or a custom usage message

---
+++ bin/ke.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set MY_PATH="%~dp0..\Kivy-Easier.exe"
+
+if "%1" == "wadb" (
+    for /f "tokens=2,* delims= " %%a in ("%*") do set ALL_BUT_SECOND=%%b
+    if "%2" == "run" (
+        %MY_PATH% run /home/ke/scripts/wadb-run.sh %ALL_BUT_SECOND%
+    ) else (
+        %MY_PATH% run /home/ke/scripts/wadb-settings.sh %ALL_BUT_SECOND%
+    )
+) else (
+    if "%1" == "help" (
+        %MY_PATH% run cat /home/helps/ke.txt
+    ) else (
+        for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
+        %MY_PATH% %ALL_BUT_FIRST%
+    )
+)
