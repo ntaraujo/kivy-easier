55b764a27d9e42ff6627ec4e2bef76c7958eab84
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2020-12-02 22:46:11 -0300

    changed to live up to the root user

--- bin/ke.bat
+++ bin/ke.bat
@@ -7,13 +7,13 @@ if "%1" == "wadb" (
     call set _tail=%%_tail:*%1=%%
     call set _tail=%%_tail:*%2=%%
     if "%2" == "run" (
-        %MY_PATH% run "/home/ke/scripts/wadb-run.sh !_tail!"
+        %MY_PATH% run "/root/scripts/wadb-run.sh !_tail!"
     ) else (
-        %MY_PATH% run "/home/ke/scripts/wadb-settings.sh !_tail!"
+        %MY_PATH% run "/root/scripts/wadb-settings.sh !_tail!"
     )
 ) else (
     if "%1" == "help" (
-        %MY_PATH% run "cat /home/ke/helps/ke.txt"
+        %MY_PATH% run "cat /root/helps/ke.txt"
     ) else (
         %MY_PATH% %*
     )
